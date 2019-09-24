7917a315ce3ebacc24d81d834f611fbce91cae3a
Fitri <Fitri@DESKTOP-5GMG7NE>
2019-09-24 07:00:03 -0700

    Latian1-Biodata

---
+++ src/latihan1/Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author 
+ * Nama         : Anindira Dina Fitriani
+ * Kelas        : IF 11 - K
+ * Deskripsi    : Menampilkan Value ke Layar
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------------");
+        System.out.println("NAMA\t\t: Anindira Dina Fitriani");
+        System.out.println("ALAMAT\t\t: Jl. Tubagus Ismail no.52");
+        System.out.println("TEMPAT LAHIT\t: Lampung");
+        System.out.println("TANGGAL LAHIR\t: 29 Juni 2000");
+        System.out.println("-------------------------");
+        System.out.println("HOBI\t\t: Nonton Drakor");
+        System.out.println("MAKES\t\t: Tteokbokki");
+        System.out.println("MIKES\t\t: Jus Jeruk");
+    }
+    
+}
